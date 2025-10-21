5e31bb14a76e17f92291eaa16e9fea80b56d305d
Ezzahir <anassezzahir@gmail.com>
2025-10-21 10:51:31 +0100

    add icons to navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,28 +19,15 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--outline button--secondary button--lg"
-            to="/docs/intro"
-            style={{
-              backgroundColor: 'rgba(255, 255, 255, 0.9)',
-              borderColor: 'var(--ifm-color-primary)',
-              color: 'var(--ifm-color-primary-darkest)',
-              fontWeight: 'bold',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)'
-            }}>
-            Get Started
-          </Link>
-          <Link
             className="button button--primary button--lg"
-            to="/docs/installation/download-install"
+            to="/docs/intro"
             style={{
               marginLeft: '1rem',
               backgroundColor: 'var(--ifm-color-primary)',
               borderColor: 'var(--ifm-color-primary-darker)',
               fontWeight: 'bold',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)'
             }}>
-            Download & Install
+            Get Started
           </Link>
         </div>
       </div>
